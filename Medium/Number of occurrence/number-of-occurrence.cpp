db65a164b82f24264cea5085e6864f715b87deae
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2023-08-20 10:07:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -10,54 +10,52 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int count(int v[], int n, int x) {
+	int count(int arr[], int n, int x) {
 	    // code here
-	     long int left=-1;
-        long int right=-1;
-    long  int start=0; 
-    long   int end=n-1;
-        while(start<=end)
-        {
-            long int mid=start + (end-start)/2;
-            if(v[mid]==x)
-            {
-                left=mid;
-                end=mid-1;
-            }
-            else if(v[mid]<x)
-            {
-                start=mid+1;
-            }
-            else
-            {
-                end=mid-1;
-            }
-        }
-        
-      start=0; 
-     end=n-1;
-        while(start<=end)
-        {
-            long int mid=start + (end-start)/2;
-            if(v[mid]==x)
-            {
-                right=mid;
-                start=mid+1;
-            }
-            else if(v[mid]<x)
-            {
-                start=mid+1;
-            }
-            else
-            {
-                end=mid-1;
-            }
-        }
-        if(left==-1)
-        {
-            return 0;
-        }
-        return right-left +1;
+	    int start=0;
+	    int end=n-1;
+	    int left=-1;
+	    int right=-1;
+	    while(start<=end)
+	    {
+	        int mid=start+(end-start)/2;
+	        if(arr[mid]==x)
+	        {
+	            left=mid;
+	            end=mid-1;
+	        }
+	        else if(arr[mid]>x)
+	        {
+	            end=mid-1;
+	        }
+	        else
+	        {
+	            start=mid+1;
+	        }
+	    }
+	    start=0;
+	    end=n-1;
+	    while(start<=end)
+	    {
+	        int mid=start+(end-start)/2;
+	        if(arr[mid]==x)
+	        {
+	            right=mid;
+	            start=mid+1;
+	        }
+	        else if(arr[mid]>x)
+	        {
+	            end=mid-1;
+	        }
+	        else
+	        {
+	            start=mid+1;
+	        }
+	    }
+	    if(left!=-1)
+	    return (right-left+1);
+	    else
+	    return 0;
 	}
 };
 
