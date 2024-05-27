fb5aa0bb700fc8aaed8307a228a70088fc09dc86
Sachin Verma <97831442+sachin27verma@users.noreply.github.com>
2024-05-27 15:25:13 +0530

    Updated Solution - GfG to GitHub

--- Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Solution{
 
 	public:
+
 	int helper(int arr[], int n , int sum)
 	{
 	    if(n==0 || sum==0)
@@ -24,31 +25,34 @@ class Solution{
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
-          long long dp[n+1][sum+1];
-        long long mod=1e9+7;
-        int zero=0;
-        for(int i=0;i<n;i++)
+        int mod=1e9+7;
+        int t[n+1][sum+1];
+        for(int i=0; i<n+1; i++)
         {
-            if(arr[i]==0)
-            zero++;
-        }
-        for(int i=0;i<=n;i++)
-        dp[i][0]=1;
-        for(int i=1;i<=sum;i++)
-        dp[0][i]=0;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=sum;j++)
+            for(int j=0; j<sum+1; j++)
             {
-                if(arr[i-1]<=j && arr[i-1]!=0)
-                {
-                    dp[i][j]=((dp[i-1][j]%mod)+(dp[i-1][j-arr[i-1]]%mod))%mod;
-                }
-                else
-                dp[i][j]=dp[i-1][j]%mod;
+                if(i==0)
+                    t[i][j]=0;
+                if(j==0)
+                    t[i][j]=1;
             }
         }
-        return pow(2,zero)*dp[n][sum]; 
+        
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=0; j<sum+1; j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    t[i][j]=(t[i-1][j-arr[i-1]]%mod+t[i-1][j]%mod)%mod;
+                }
+                else
+                {
+                    t[i][j]=t[i-1][j]%mod;
+                }
+            }
+        }
+        return t[n][sum]%mod;
 	}
 	  
 };
