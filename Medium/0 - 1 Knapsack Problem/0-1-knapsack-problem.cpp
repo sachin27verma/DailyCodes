4e02bff20261f415a7992d77de0bb47dc109f217
Sachin Verma <97831442+sachin27verma@users.noreply.github.com>
2024-05-27 13:26:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -30,8 +30,34 @@ class Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       memset(t,-1,sizeof(t));
-       return knaprec(W,wt,val,n);
+    //   memset(t,-1,sizeof(t));
+    //   return knaprec(W,wt,val,n);
+    int t[n+1][W+1];
+    for(int i=0; i<n+1; i++)
+    {
+        for(int j=0; j<W+1; j++)
+        {
+            if(i==0 || j==0)
+            {
+                t[i][j]=0;
+            }
+        }
+    }
+    
+    for(int i=1; i<n+1; i++)
+    {
+        for(int j=1; j<W+1; j++)
+        {
+            if(wt[i-1]<=j)
+            {
+                t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
+            }
+            else
+                t[i][j]=t[i-1][j];
+        }
+    }
+    return t[n][W];
+    
     }
 };
 
